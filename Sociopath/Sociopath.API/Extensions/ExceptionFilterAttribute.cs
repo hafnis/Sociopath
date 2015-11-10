0a9e605db4a5269605a495ccdbde70544c19e941
hafnis <hafnis@gmail.com>
2015-11-11 00:14:40 +0200

    Added dummy exception logging

---
+++ Sociopath/Sociopath.API/Extensions/ExceptionFilterAttribute.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Net.Http;
+using System.Net;
+using System.Web.Http.Filters;
+using NLog;
+
+namespace Sociopath.API.Extensions
+{
+    public class ExceptionAttribute : ExceptionFilterAttribute
+    {
+        private static Logger logger = LogManager.GetCurrentClassLogger();
+
+        public override void OnException(HttpActionExecutedContext actionExecutedContext)
+        {
+            logger.Error(actionExecutedContext.Exception);
+            actionExecutedContext.Response = actionExecutedContext.Request.CreateErrorResponse(HttpStatusCode.InternalServerError, "Internal server error.");
+        }
+    }
+}
